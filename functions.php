167c1096f71d18cffb1950d21f2136a4f090d3f5
Matheus <matheus@seox.com.br>
2024-10-28 18:17:10 -0300

    Implementação do Menu da página.

--- functions.php
+++ functions.php
@@ -12,3 +12,15 @@ function menus () {
 }
 
 add_action('init', "menus");
+
+function custom_logo() {
+    // Suporte ao upload de logo
+    add_theme_support( 'custom-logo', array(
+        'height'      => 74,     // Altura recomendada do logo
+        'width'       => 267,    // Largura recomendada do logo
+        'flex-height' => true,   // Permitir altura flexível
+        'flex-width'  => true,   // Permitir largura flexível
+    ) );
+}
+
+add_action( 'after_setup_theme', "custom_logo" );
